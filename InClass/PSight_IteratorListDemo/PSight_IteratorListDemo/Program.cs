17298ede72bfe4c91a263123bc7cea5e528d007d
MCampbell08 <WaffleDefend29@gmail.com>
2018-04-25 19:40:39 -0600

    Demos

---
+++ InClass/PSight_IteratorListDemo/PSight_IteratorListDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSight_IteratorDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BikeRepository repo = new BikeRepository();
+
+            repo.addBike("Cervelo");
+            repo.addBike("Scott");
+            repo.addBike("Fuji");
+
+            Iterator<string> bikeIterator = repo.iterator();
+
+            while (bikeIterator.HasNext())
+            {
+                Console.WriteLine(bikeIterator.Next());
+            }
+        }
+    }
+}
