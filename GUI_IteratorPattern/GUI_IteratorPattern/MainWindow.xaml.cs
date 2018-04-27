78e75ed9387c752364780f57455e28162f9837cc
MCampbell08 <WaffleDefend29@gmail.com>
2018-04-26 22:19:13 -0600

    Continuation on GUI

--- GUI_IteratorPattern/GUI_IteratorPattern/MainWindow.xaml.cs
+++ GUI_IteratorPattern/GUI_IteratorPattern/MainWindow.xaml.cs
@@ -20,9 +20,38 @@ namespace GUI_IteratorPattern
     /// </summary>
     public partial class MainWindow : Window
     {
+        Questionnaire questionnaire;
+        QuestionSection questionSections;
+        QuestionSubSections questionSubSections;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void InitializeQuestionModels()
+        {
+            questionnaire = new Questionnaire() { Sections = new List<QuestionComponent>() };
+            questionSections = new QuestionSection(){ SubSections = new List<QuestionComponent>() };
+            questionSubSections = new QuestionSubSections(){ Questions = new List<QuestionComponent>() } ;
+        }
+
+        private void AddSectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(sectionInput.Text != "")
+            {
+                var result = questionnaire.Sections.Where(x => (QuestionSection)x.);
+            }
+        }
+
+        private void AddSubSectionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddQuestionButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
