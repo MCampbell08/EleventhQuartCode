c8eefa298d937f891ac48d856bb28608b3ff7275
MCampbell08 <WaffleDefend29@gmail.com>
2018-04-29 18:26:22 -0600

    added timer handler to xaml.cs file

--- GUI_StatePattern/GUI_StatePattern/MainWindow.xaml.cs
+++ GUI_StatePattern/GUI_StatePattern/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,9 +21,26 @@ namespace GUI_StatePattern
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const double INTERTVAL = 3000;
+        private Timer timer = new Timer(INTERTVAL);
         public MainWindow()
         {
             InitializeComponent();
+            timer.Elapsed += OnTimedEvent;
+            timer.Enabled = true;
+        }
+        public void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            if(timer.Interval >= INTERTVAL)
+            {
+
+            }
+        }
+
+        public enum LightState{
+            GREEN,
+            YELLOW,
+            RED
         }
     }
 }
